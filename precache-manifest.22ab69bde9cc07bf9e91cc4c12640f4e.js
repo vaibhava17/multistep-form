1f09c416a71f647319b3f4049d4a42dad47092f3
vaibhava17 <iamvaibhav.agarwal@gmail.com>
2022-08-24 20:38:54 +0530

    Updates

---
+++ precache-manifest.22ab69bde9cc07bf9e91cc4c12640f4e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "152afc91f6206692dd7d",
+    "url": "/multistep-form/static/css/main.dc27ffb8.chunk.css"
+  },
+  {
+    "revision": "152afc91f6206692dd7d",
+    "url": "/multistep-form/static/js/main.152afc91.chunk.js"
+  },
+  {
+    "revision": "156921dc609b6f36a167",
+    "url": "/multistep-form/static/js/1.156921dc.chunk.js"
+  },
+  {
+    "revision": "647d29cd2e0b37a6b7a2",
+    "url": "/multistep-form/static/css/2.b3757d07.chunk.css"
+  },
+  {
+    "revision": "647d29cd2e0b37a6b7a2",
+    "url": "/multistep-form/static/js/2.647d29cd.chunk.js"
+  },
+  {
+    "revision": "46d6ee608bdaf30a53ec",
+    "url": "/multistep-form/static/js/runtime~main.46d6ee60.js"
+  },
+  {
+    "revision": "fb71aea9299f40b0d8c1c236e1636da1",
+    "url": "/multistep-form/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/multistep-form/precache-manifest.22ab69bde9cc07bf9e91cc4c12640f4e.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/multistep-form/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
